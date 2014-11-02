a0d72c66593af84a3d9cdbe9dbe961ae14e08847
s86xu <s86xu@uwaterloo.ca>
2014-11-01 21:59:41 -0400

    try

--- control.py
+++ control.py
@@ -1,20 +1,49 @@
 # Hackathon 2014
 
+import myo
+
+from myo.six import print_
 from myro import *
 
-init(raw_input("What is the COM port? "))
+class Listener(myo.DeviceListener):
+    #return False from any method to stop the Hub
 
-while True:
-    key = raw_input()
-    if key == "f":
-        forward()
-    elif key == "r":
-        backward()
-    elif key == "t":
-        spd = input()
-        turn(spd)
-    elif key == "s":
-        stop()
+    def on_connect(self, myo, timestamp):
+        print_("Connected to Myo")
+        myo.vibrate('short')
+        myo.request_rssi()
+
+    def on_rssi(self, myo, timestamp, rssi):
+        print_("RSSI:", rssi)
+
+    def on_event(self, event):
+        r""" Called before any of the event callbacks. """
+
+    def on_event_finished(self, event):
+        r""" Called after the respective event callbacks have been
+        invoked. This method is *always* triggered, even if one of
+        the callbacks requested the stop of the Hub. """
+
+    def on_pair(self, myo, timestamp):
+        print_('on_pair')
+
+    def on_disconnect(self, myo, timestamp):
+        print_('on_disconnect')
+
+    def on_pose(self, myo, timestamp, pose):
+        print_('on_pose', pose)
+
+    def on_orientation_data(self, myo, timestamp, orientation):
+        pass
+
+    def on_accelerometor_data(self, myo, timestamp, acceleration):
+        pass
+
+    def on_gyroscope_data(self, myo, timestamp, gyroscope):
+        pass
+
+
+init(raw_input("What is the COM port? "))
 
 
 def forward():
@@ -26,3 +55,16 @@ def backward():
 def turn(speed):
     rotate(speed)
 
+
+def main():
+    hub = myo.Hub()
+    hub.run(1000, Listener())
+
+    # Listen to keyboard interrupts and stop the
+    # hub in that case.
+    try:
+        while hub.running:
+            myo.time.sleep(0.2)
+    except KeyboardInterrupt:
+        print_("Quitting ...")
+        hub.stop(True)
